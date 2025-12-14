305bb3967a6b37a95f6dae01d8b540af24ff5691
Carter Williams <135449037+CarterWilliams2@users.noreply.github.com>
2025-12-14 15:32:00 -0500

    add Game Data Transfer Object

---
+++ cbbScoreboard/Models/GameDto.cs
@@ -0,0 +1,12 @@
+namespace cbbScoreboard.Models;
+
+public class GameDto
+{
+    public string HomeTeam { get; set; } = string.Empty;
+    public string AwayTeam { get; set; } = string.Empty;
+
+    public int HomeScore { get; set; }
+    public int AwayScore { get; set; }
+
+    public string Status { get; set; } = string.Empty;
+}
