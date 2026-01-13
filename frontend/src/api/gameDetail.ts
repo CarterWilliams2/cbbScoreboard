991296672a2e0a4c56befdb184a1bf5e75952673
Carter Williams <135449037+CarterWilliams2@users.noreply.github.com>
2026-01-13 09:59:10 -0600

    fix api call in gameDetail

--- frontend/src/api/gameDetail.ts
+++ frontend/src/api/gameDetail.ts
@@ -5,7 +5,7 @@ const API_BASE = "http://localhost:5150/api/games";
 export async function fetchGameDetail(params?: {
     gameId: string,
 }) {
-    const res = await fetch(`${API_BASE}?${params?.gameId}`);
+    const res = await fetch(`${API_BASE}/${params?.gameId}`);
 
     if (!res.ok) {
         throw new Error("Failed to fetch games");
