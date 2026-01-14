50feb2934c97c264d21da28f566c19682322af65
Carter Williams <135449037+CarterWilliams2@users.noreply.github.com>
2026-01-14 11:01:58 -0600

    set up play by play service url

--- cbbScoreboard/Services/PlayByPlayService.cs
+++ cbbScoreboard/Services/PlayByPlayService.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
 using System.Globalization;
 using System;
+using System.Reflection.Metadata;
 
 
 namespace cbbScoreboard.Services;
@@ -20,5 +21,25 @@ public class PlayByPlayService
         _httpClient = httpClient;
     }
 
-    
+    public async Task<List<PlayByPlayDto>> GetPlayByPlayAsync(string gameId)
+    {
+
+        var url = $"https://ncaa-api.henrygd.me/game/{gameId}/play-by-play";
+        var response  = await _httpClient.GetStringAsync(url);
+        using var doc = JsonDocument.Parse(response);
+
+        var plays = ParsePlayByPlay(doc);
+
+        return plays;
+
+    }
+
+    private List<PlayByPlayDto> ParsePlayByPlay(JsonDocument doc)
+    {
+        
+
+
+
+        return null;
+    } 
 }
