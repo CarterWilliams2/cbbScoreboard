b3e8e14e71de65b1fc48eeae732357a5c4e75ad9
Carter Williams <135449037+CarterWilliams2@users.noreply.github.com>
2026-01-14 18:35:00 -0600

    write front end implementation

--- frontend/src/api/playByPlay.ts
+++ frontend/src/api/playByPlay.ts
@@ -5,3 +5,18 @@ export type PlayByPlay = {
     clock: string,
     event_description: string,
 }
+
+const API_BASE = "http://localhost:5150/play-by-play";
+
+export async function fetchPLayByPlay(params: {
+    gameId: string,
+}) {
+
+    const res = await fetch(`${API_BASE}/${params.gameId}`);
+
+    if (!res.ok) {
+        throw new Error("Failed to fetch plays");
+    }
+
+    return res.json() as Promise<PlayByPlay[]>;
+}
