2fbb111cfab6dff56929458755ed71108e797056
Carter Williams <135449037+CarterWilliams2@users.noreply.github.com>
2026-01-16 10:36:48 -0600

    write helper function to convert a play into a gamestate

--- backend/Services/WinProbabilityService.cs
+++ backend/Services/WinProbabilityService.cs
@@ -11,12 +11,40 @@ public class WinProbabilityService
         _httpClient = httpClient;
     }
 
-    public async Task<WinProbabilityDto> GetWinProbabilityAsync()
+    public async Task<WinProbabilityDto> GetWinProbabilityAsync(PlayByPlayDto play)
     {
+        // convert play into a gamestate
+
         var url = "http://localhost:8000/predict";
         var response  = await _httpClient.GetStringAsync(url);
         // parse/shape response
         return null;
+    }
+
+    private GameStateDto ConvertPlayToGameState(PlayByPlayDto play)
+    {
+        var away_score = int.Parse(play.VisitorScore);
+        var home_score = int.Parse(play.HomeScore);
+        var period = int.Parse(play.PeriodNumber);
+
+        string[] clock_elements = play.Clock.Split(":");
+        var minutes = int.Parse(clock_elements[0]);
+        var seconds = int.Parse(clock_elements[1]);
+
+        var time_remaining_seconds = seconds + minutes*60;
+        if (period == 1)
+        {
+            time_remaining_seconds += 20*60;
+        }
+
+        return new GameStateDto
+        {
+          AwayScore = away_score,
+          HomeScore = home_score,
+          Period = period,
+          TimeRemainingSeconds = time_remaining_seconds,  
+        };
+
     }
 
    
