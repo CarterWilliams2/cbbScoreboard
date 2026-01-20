0c793bf68da77fcd2985a8c7f063addf39fc28fc
Carter Williams <135449037+CarterWilliams2@users.noreply.github.com>
2026-01-20 10:12:08 -0600

    get plays for a game

--- ml-service/training/fetch_data.py
+++ ml-service/training/fetch_data.py
@@ -5,7 +5,6 @@ import time
 
 def fetch_game_ids_for_date(date_string):
 
-    # get all games for a certain date
     url = f"https://ncaa-api.henrygd.me/scoreboard/basketball-men/d1/{date_string}"
     game_ids = []
 
@@ -25,6 +24,8 @@ def fetch_game_ids_for_date(date_string):
                 game_ids.append(game_id)
         
         print(f"{len(game_ids)} completed games")
+
+        return game_ids
         
 
         
@@ -32,14 +33,41 @@ def fetch_game_ids_for_date(date_string):
         print(f"Error fetching games for {date_string}: {e}")
         return []
 
-    # grab all of the game ids
+def fetch_plays_for_game_id(game_id):
 
-    #return the ids
+    url = f"https://ncaa-api.henrygd.me/game/{game_id}/play-by-play"
+    plays = []
 
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
 
-    return None
+        periods = data.get('periods', [])
+        for period in periods:
+            period_plays = period.get('playbyplayStats', [])
+            for play in period_plays:
+                plays.append(play)
+        
+        print(f"Found {len(plays)} plays for game: {game_id}")
+
+        return plays
+        
+    except Exception as e:
+        print(f"Error fetching plays for {game_id}: {e}")
+        return []
+
 
 if __name__ == "__main__":
-   date_string = "2026/01/19"
+    date_string = "2026/01/19"
 
-   fetch_game_ids_for_date(date_string)
+    game_ids = fetch_game_ids_for_date(date_string)
+
+    all_plays = []
+    for game_id in game_ids:
+        game_plays = fetch_plays_for_game_id(game_id)
+        all_plays.extend(game_plays)
+        time.sleep(0.5)
+    
+
+
