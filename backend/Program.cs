a9de838cddaa2214e40645bc63f0d93340d30429
Carter Williams <135449037+CarterWilliams2@users.noreply.github.com>
2026-01-21 08:15:13 -0600

    try to fix deployment issues

--- backend/Program.cs
+++ backend/Program.cs
@@ -3,31 +3,20 @@ using cbbScoreboard.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.WebHost.ConfigureKestrel(options =>
-{
-    var port = Environment.GetEnvironmentVariable("PORT");
-    if (port != null)
-    {
-        options.ListenAnyIP(int.Parse(port));
-    }
-});
-
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("frontend", policy =>
     {
         policy
-            .WithOrigins("http://localhost:5173")
-            .WithOrigins("http://cbb-scoreboard-frontend.s3-website.us-east-2.amazonaws.com/")
+            .WithOrigins(
+                "http://localhost:5173",
+                "http://cbb-scoreboard-frontend.s3-website.us-east-2.amazonaws.com"
+            )
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
 });
 
-
-
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
 builder.Services.AddMemoryCache();
@@ -41,12 +30,11 @@ var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
+
 app.UseCors("frontend");
-app.UseHttpsRedirection();
+
+// app.UseHttpsRedirection();
 
 app.MapControllers();
-
-var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
-app.Urls.Add($"http://*:{port}");
 
 app.Run();
