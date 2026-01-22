29dcca431be49a50722c916affbde487d5bd3b3f
Carter Williams <135449037+CarterWilliams2@users.noreply.github.com>
2026-01-22 09:20:41 -0600

    prepare for CI/CD for backend

--- backend/obj/Debug/net10.0/cbbScoreboard.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/cbbScoreboard.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cbbScoreboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25e435bdc1a2727344a23024b8d6163db2d448a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e154a7348caccf89e18043020e01f6430be42160")]
 [assembly: System.Reflection.AssemblyProductAttribute("cbbScoreboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cbbScoreboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
