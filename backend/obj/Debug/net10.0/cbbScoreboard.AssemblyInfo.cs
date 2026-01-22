5cb32e73fb715b638e3c00db416a902a2aef1275
Carter Williams <135449037+CarterWilliams2@users.noreply.github.com>
2026-01-21 20:09:19 -0600

    update env vari

--- backend/obj/Debug/net10.0/cbbScoreboard.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/cbbScoreboard.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cbbScoreboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d57152bfeed65ad3cb56bfabb7a10e582ed5eab0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3fde70141fda382cb971dbde408d0b4d39cb0c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("cbbScoreboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cbbScoreboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
